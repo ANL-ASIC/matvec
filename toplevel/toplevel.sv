a2db49e9f956cf91ed34f16cb2ff6202736ea596
Ramirasheedi <76470187+Ramirasheedi@users.noreply.github.com>
2025-06-02 16:49:15 -0500

    Update toplevel.sv

--- toplevel/toplevel.sv
+++ toplevel/toplevel.sv
@@ -1,6 +1,6 @@
-`include "../SRAM/SRAM.sv"
-`include "../sram_addr_fsm/sram_addr_fsm.sv"
-`include "../hw/matvec.sv"
+`include "../matvec/SRAM/SRAM.sv"
+`include "../matvec/sram_addr_fsm/sram_addr_fsm.sv"
+`include "../matvec/hw/matvec.sv"
 
 module top_level #(
     parameter pixel_data_width             = 12,
