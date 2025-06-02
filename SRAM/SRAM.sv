b6e52e1d6c640cf04758044224f741d41d614451
Ramirasheedi <76470187+Ramirasheedi@users.noreply.github.com>
2025-06-02 15:45:04 -0500

    Update SRAM.sv

--- SRAM/SRAM.sv
+++ SRAM/SRAM.sv
@@ -10,8 +10,8 @@ module SRAM #(
     input  logic write_enable,
     input  logic [$clog2(num_row_per_frame)-1:0] addr,
     input  logic [$clog2(num_row_per_frame)-1:0] write_addr,
-    input  logic [number_of_columns_per_frame-1:0][K-1:0][weight_width-1:0] write_data, 
-    output logic [number_of_columns_per_frame-1:0][K-1:0][weight_width-1:0] data_out    
+    input  logic [weight_width-1:0][num_col_per_frame-1:0][K-1:0] write_data, 
+    output logic [weight_width-1:0][num_col_per_frame-1:0][K-1:0] data_out    
 );
 
     genvar k;
