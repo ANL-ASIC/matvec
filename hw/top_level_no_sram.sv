0b645349336294a2215e82af2cf4cb4a4311c9e2
Ramirasheedi <76470187+Ramirasheedi@users.noreply.github.com>
2025-06-18 09:53:41 -0500

    Update top_level_no_sram.sv

--- hw/top_level_no_sram.sv
+++ hw/top_level_no_sram.sv
@@ -1,52 +1,51 @@
-//`include "/local/ralsaeed/top/matvec/SRAM/SRAM.sv"
-//`include "/local/ralsaeed/top/matvec/SRAM/SRAM_bank.sv"
-//`include "/local/ralsaeed/top/matvec/SRAM/SRAM_block.sv"
-`include "/local/ralsaeed/top/matvec/sram_addr_fsm/sram_addr_fsm.sv"
-`include "/local/ralsaeed/top/matvec/hw/matvec.sv"
-`include "/local/ralsaeed/top/matvec/hw/FPpack.sv"
-`include "/local/ralsaeed/top/matvec/hw/FPunpack.sv"
-`include "/local/ralsaeed/top/matvec/hw/FPround.sv"
-`include "/local/ralsaeed/top/matvec/hw/FPnormalizeAdd.sv"
-`include "/local/ralsaeed/top/matvec/hw/FPnormalizeMul.sv"
-`include "/local/ralsaeed/top/matvec/hw/int_to_float.sv"
-`include "/local/ralsaeed/top/matvec/hw/FPmul.sv"
-`include "/local/ralsaeed/top/matvec/hw/int_fp_mult.sv"
-`include "/local/ralsaeed/top/matvec/hw/weight_mult.sv"
-`include "/local/ralsaeed/top/matvec/hw/FPadd.sv"
-`include "/local/ralsaeed/top/matvec/hw/accumulator.sv"
-
+`include "SRAM.sv"
+`include "SRAM_bank.sv"
+`include "SRAM_block.sv"
+`include "sram_addr_fsm.sv"
+`include "matvec.sv"
+`include "FPpack.sv"
+`include "FPunpack.sv"
+`include "FPround.sv"
+`include "FPnormalizeAdd.sv"
+`include "int_to_float.sv"
+`include "int_fp_mult.sv"
+`include "weight_mult.sv"
+`include "FPadd.sv"
+`include "accumulator.sv"
 
 module top_level_no_sram #(
     parameter pixel_data_width             = 12,
-    parameter number_of_columns_per_frame  = 192,
+    parameter number_of_columns_per_frame  = 24,
     parameter number_of_rows_per_frame     = 168,
     parameter K                            = 24,
-    parameter EWIDTH                       = 8,
-    parameter SIGWIDTH                     = 23
+    parameter EWIDTH                       = 5,
+    parameter SIGWIDTH                     = 10
 )(
     input  logic clk,
     input  logic reset,
     input  logic SRO,
-    input  logic dv, 
-    input  logic [K-1:0][number_of_columns_per_frame-1:0][EWIDTH + SIGWIDTH:0] read_data,
+    input  logic dv,
 
     // SRAM init interface
     //input  logic write_enable,
     //input  logic [$clog2(number_of_rows_per_frame)-1:0] write_addr,
     //input  logic [K-1:0][number_of_columns_per_frame-1:0][EWIDTH + SIGWIDTH:0] write_data,
 
+    input logic [K-1:0][number_of_columns_per_frame-1:0][EWIDTH + SIGWIDTH:0] read_data, // packed
+
     // Main input/output
-    input  logic [number_of_columns_per_frame-1:0][pixel_data_width-1:0] pixel_data,     
-    output logic [K - 1:0][EWIDTH + SIGWIDTH:0] result,
-    output logic [$clog2(number_of_rows_per_frame)-1:0] addr_out  
+    input  logic [number_of_columns_per_frame-1:0][pixel_data_width-1:0] pixel_data,
+    output logic [$clog2(number_of_rows_per_frame)-1:0] addr_out,
+    output logic [K - 1:0][EWIDTH + SIGWIDTH:0] result
 );
 
     localparam weight_width = EWIDTH + SIGWIDTH + 1;
-    
+
     // -----------------------------------
     // Address FSM
     // -----------------------------------
     //logic [$clog2(number_of_rows_per_frame)-1:0] addr_out;
+    logic do_acc;
 
     sram_addr_fsm #(
         .SRAM_DEPTH(number_of_rows_per_frame)
@@ -54,8 +53,9 @@ module top_level_no_sram #(
         .clk(clk),
         .reset(reset),
         .SRO(SRO),
-        .addr_out(addr_out),
-        .dv(dv)
+        .dv(dv),
+        .do_acc(do_acc),
+        .addr_out(addr_out)
     );
 
     // -----------------------------------
@@ -64,17 +64,17 @@ module top_level_no_sram #(
     //logic [K-1:0][number_of_columns_per_frame-1:0][weight_width-1:0] read_data; // packed
 
     //SRAM #(
-     //   .K(K),
-      //  .num_col_per_frame(number_of_columns_per_frame),
-      //  .weight_width(weight_width),
-       // .num_row_per_frame(number_of_rows_per_frame)
+    //    .K(K),
+    //    .num_col_per_frame(number_of_columns_per_frame),
+    //    .weight_width(weight_width),
+    //    .num_row_per_frame(number_of_rows_per_frame)
     //) sram_inst (
-      //  .clk(clk),
-       // .write_enable(write_enable),
-        //.write_addr(write_addr),
-        //.addr(addr_out),
-        //.write_data(write_data),
-        //.data_out(read_data)
+    //    .clk(clk),
+    //    .write_enable(write_enable),
+    //    .write_addr(write_addr),
+    //    .addr(addr_out),
+    //    .write_data(write_data),
+    //    .data_out(read_data)
     //);
 
     // -----------------------------------
@@ -88,7 +88,8 @@ module top_level_no_sram #(
         .N(number_of_columns_per_frame)
     ) mac (
         .clk(clk),
-        .acc_rst(reset),
+        .do_acc(do_acc),
+        .dv(dv),
         .in(pixel_data),
         .weights(read_data),
         .out(result)
