63826c8134eba496dccf9504aa3dfbfce8904c56
Ramirasheedi <76470187+Ramirasheedi@users.noreply.github.com>
2025-07-14 10:55:54 -0500

    Update SRAM.sv

--- hw/SRAM.sv
+++ hw/SRAM.sv
@@ -4,16 +4,14 @@ module SRAM #(
     parameter K = 10,
     parameter num_col_per_frame = 192,
     parameter weight_width = 12,
-    parameter num_row_per_frame = 168,
-    parameter num_weights_per_word = 12,
-    parameter freq_factor = 4
+    parameter num_row_per_frame = 168
 ) (
     input  logic clk,
     input  logic write_enable,
-    input  logic [$clog2(freq_factor*num_row_per_frame)-1:0] addr,
-    input  logic [$clog2(freq_factor*num_row_per_frame)-1:0] write_addr,
-    input  logic [K-1:0][(num_col_per_frame / (num_weights_per_word*freq_factor))-1:0][(num_weights_per_word*weight_width)-1:0] write_data, 
-    output logic [K-1:0][(num_col_per_frame / (num_weights_per_word*freq_factor))-1:0][(num_weights_per_word*weight_width)-1:0] data_out    
+    input  logic [$clog2(num_row_per_frame)-1:0] addr,
+    input  logic [$clog2(num_row_per_frame)-1:0] write_addr,
+    input  logic [K-1:0][num_col_per_frame-1:0][weight_width-1:0] write_data, 
+    output logic [K-1:0][num_col_per_frame-1:0][weight_width-1:0] data_out    
 );
 
     genvar k;
