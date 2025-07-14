1e27f4d51a89287872fdf93eb6278763ae31b419
Ramirasheedi <76470187+Ramirasheedi@users.noreply.github.com>
2025-07-14 10:56:24 -0500

    Update SRAM_bank.sv

--- hw/SRAM_bank.sv
+++ hw/SRAM_bank.sv
@@ -3,21 +3,19 @@
 module SRAM_bank #(
     parameter weight_width = 12,
     parameter num_row_per_frame = 12,
-    parameter num_col_per_frame = 4,
-    parameter num_weights_per_word = 12,
-    parameter freq_factor = 4
+    parameter num_col_per_frame = 4
 ) (
     input  logic clk,
     input  logic write_enable,
-    input  logic [$clog2(freq_factor*num_row_per_frame)-1:0] addr,
-    input  logic [$clog2(freq_factor*num_row_per_frame)-1:0] write_addr,
-    input  logic [(num_col_per_frame / (num_weights_per_word*freq_factor))-1:0][(num_weights_per_word*weight_width)-1:0] write_data,
-    output logic [(num_col_per_frame / (num_weights_per_word*freq_factor))-1:0][(num_weights_per_word*weight_width)-1:0] data_out   
+    input  logic [$clog2(num_row_per_frame)-1:0] addr,
+    input  logic [$clog2(num_row_per_frame)-1:0] write_addr,
+    input  logic [num_col_per_frame-1:0][weight_width-1:0] write_data,
+    output logic [num_col_per_frame-1:0][weight_width-1:0] data_out   
 );
 
     genvar i;
     generate
-        for (i = 0; i < (num_col_per_frame / (num_weights_per_word*freq_factor)); i++) begin : col_gen
+        for (i = 0; i < num_col_per_frame; i++) begin : col_gen
             SRAM_block #(
                 .weight_width(weight_width),
                 .num_row_per_frame(num_row_per_frame)
